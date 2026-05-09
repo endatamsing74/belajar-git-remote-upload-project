0f7624115b892c3eb41adc9df457cece57c45242
= <almoest.enda282@ymail.com>
2026-05-09 09:23:50 +0700

    menambah baris ul li

--- index.php
+++ index.php
@@ -12,6 +12,11 @@
     <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate ipsam quis voluptatibus voluptatum, laborum qui, corrupti illo optio natus, dolorem dicta accusamus deleniti adipisci soluta in autem illum nesciunt error?
     </p>
+
+    <ul>
+        <li>tes 1</li>
+        <li>tes 2</li>
+    </ul>
 </body>
 
 </html>
